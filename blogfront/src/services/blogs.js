a295514b1a0ec6ee22ea4f9d4c1003acba120d46
Sami <samialeksi.tuomela@gmail.com>
2024-01-12 16:50:02 +0200

    fixed a lint issue preventing build from happening

--- blogfront/src/services/blogs.js
+++ blogfront/src/services/blogs.js
@@ -46,4 +46,5 @@ const deleteBlog = async (blog) => {
 }
 
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, create, addLike, addComment, deleteBlog, setToken }
