0da979e968f2b3777cfadefe31ba6ac1dc909849
Sami <samialeksi.tuomela@gmail.com>
2024-01-12 19:32:29 +0200

    reduced cross-env based activation for that route

--- blogbackend/app.js
+++ blogbackend/app.js
@@ -9,16 +9,16 @@ const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose
-    .connect(config.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        logger.info('connected to mongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connection to mongoDB: ', error.message)
-    })
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info('connected to mongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to mongoDB: ', error.message)
+  })
 
 app.use(cors())
 app.use(express.static('build'))
@@ -33,10 +33,9 @@ app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/comments', commentsRouter)
 // eslint-disable-next-line no-undef
-if (process.env.NODE_ENV === 'test') {
-    const testingRouter = require('./controllers/blogsTestReset')
-    app.use('/api/testing', testingRouter)
-}
+
+const testingRouter = require('./controllers/blogsTestReset')
+app.use('/api/testing', testingRouter)
 
 app.use(middleware.requestLogger)
 app.use(middleware.unknownEndpoint)
