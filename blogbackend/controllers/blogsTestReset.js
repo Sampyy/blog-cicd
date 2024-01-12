3b0af44ff537604205a323a65243328b58a99dae
Sami <samialeksi.tuomela@gmail.com>
2024-01-12 18:59:30 +0200

    fixed lint

--- blogbackend/controllers/blogsTestReset.js
+++ blogbackend/controllers/blogsTestReset.js
@@ -3,10 +3,10 @@ const Blog = require('.././models/blog')
 const User = require('.././models/user')
 
 router.post('/reset', async (request, response) => {
-  await Blog.deleteMany({})
-  await User.deleteMany({})
+    await Blog.deleteMany({})
+    await User.deleteMany({})
 
-  response.send('db reset').status(200).end()
+    response.send('db reset').status(200).end()
 })
 
 // eslint-disable-next-line no-undef
